dc9260c3412fe33bf7f5d763bc5c8dfe084f860d
shiqing2025 <shiqing2025@foxmail.com>
2024-06-06 22:52:14 +0800

    206. 反转链表

---
+++ 206. 反转链表.ts
@@ -0,0 +1,40 @@
+/** 
+ * 206. 反转链表
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let pre = <ListNode | null>null
+  let cur = head;
+
+  while (cur) {
+    let temp = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = temp;
+  }
+  return pre;
+};
