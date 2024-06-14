44554a25d642fd6cfac071e1745c91c7be67083a
shiqing2025 <shiqing2025@foxmail.com>
2024-06-14 15:17:02 +0800

    344. 反转字符串

---
+++ 344. 反转字符串.ts
@@ -0,0 +1,24 @@
+/** 
+ * 344. 反转字符串
+ * https://leetcode.cn/problems/reverse-string/description/
+ * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+*/
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (right > left) {
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
+    }
+};
+
+// Test
+const input: string[] = ["h", "e", "l", "l", "o"];
+reverseString(input);
+console.log(input); // ["o", "l", "l", "e", "h"]
