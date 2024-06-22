a110d781693c7519cf94154275ee3bb8e55b674a
shiqing2025 <shiqing2025@foxmail.com>
2024-06-22 16:58:43 +0800

    2723. 两个 Promise 对象相加

---
+++ 2723. 两个 Promise 对象相加.ts
@@ -0,0 +1,33 @@
+/** 
+ * 2723. 两个 Promise 对象相加
+ * https://leetcode.cn/problems/add-two-promises/description/?envType=study-plan-v2&envId=30-days-of-javascript
+ * 
+ * 给定两个 promise 对象 promise1 和 promise2，返回一个新的 promise。
+ * promise1 和 promise2 都会被解析为一个数字。
+ * 返回的 Promise 应该解析为这两个数字的和。
+*/
+
+type P = Promise<number>;
+
+function add(promise1: P, promise2: P): P {
+  return Promise.allSettled([promise1, promise2]).then(([a, b]) => {
+    console.log('a', a);
+    console.log('b', b);
+    return a.status === 'fulfilled' && b.status === 'fulfilled' ? Promise.resolve(a.value + b.value) : Promise.reject('error');
+  });
+  // return Promise.all([promise1, promise2]).then(([a, b]) => {
+  //   console.log('a', a);
+  //   console.log('b', b);
+  //   return a + b;
+  // })
+}
+
+
+// test
+const p1 = Promise.resolve(1);
+const p2 = Promise.resolve(2);
+
+
+add(p1, p2).then((res) => {
+  console.log(res);
+});
